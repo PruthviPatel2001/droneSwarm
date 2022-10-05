bb7b021f9ca555c31cb7a029c7763bda4f5adbf9
PruthviPatel2001 <patelpruthvi.01221@gmail.com>
2022-10-05 18:04:54 +0530

    tailwind fix

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -4,11 +4,27 @@ import React from "react";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Home</Text>
-      </View>
-    </SafeAreaView>
+    <View className=" bg-[#17233B] flex-1">
+      <SafeAreaView>
+        <View className="flex justify-between flex-row p-4">
+          <Text
+            className="text-white text-3xl"
+            style={{ fontFamily: "RobotoBold" }}
+          >
+            StrandAID
+          </Text>
+          <Text>hello</Text>
+        </View>
+        <View className="mt-4">
+          <Text>
+            Hello,
+          </Text>
+          <Text>
+            Admin: {"<Nzame />"}
+          </Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
