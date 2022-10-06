3bf3acca917c0df6077e53b8591eb7cb67e93eab
Shubham Salunke <shubhamdeepak007@gmail.com>
2022-10-06 19:45:25 +0530

    Switched to Formik

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -14,6 +14,7 @@ import {
 import React from "react";
 import icons from "../constants/icons";
 import {useNavigation} from "@react-navigation/native";
+import {Formik} from "formik";
 
 const Profile = () => {
 
@@ -36,72 +37,102 @@ const Profile = () => {
                     className='mt-6'
                 />
             </View>
-            <View className='mx-4 text-left mt-6'>
-                <Text className='text-sm text-gray-500'>Name</Text>
-                <View className='flex flex-row justify-center items-center w-full border-solid border-b-2 border-gray-200'>
-                    <TextInput
-                        className='flex flex-row w-full'
-                        value=''
-                    />
-                    <Image
-                        source={icons.edit}
-                        style={{
-                            width: 20,
-                            height: 20,
-                        }}
-                        resizeMode='contain'
-                    />
-                </View>
-                <Text className='text-sm text-gray-500 mt-4'>Email</Text>
-                <Text className='text-xl border-solid border-b-2 border-gray-200'>shubhamdeepak007@gmail.com</Text>
-                <Text className='text-sm text-gray-500 mt-4'>Date of Birth</Text>
-                <View className='flex flex-row justify-center items-center w-full border-solid border-b-2 border-gray-200'>
-                    <TextInput
-                        className='flex flex-row w-full'
-                        value=''
-                    />
-                    <Image
-                        source={icons.edit}
-                        style={{
-                            width: 20,
-                            height: 20,
-                        }}
-                        resizeMode='contain'
-                    />
-                </View>
-                <Text className='text-sm text-gray-500 mt-4'>Account Type</Text>
-                <Text className='text-xl border-solid border-b-2 border-gray-200'>Moderator</Text>
-                <Text className='text-sm text-gray-500 mt-4'>Fast Login</Text>
-                <View className='flex flex-row justify-between border-solid border-b-2 border-gray-200'>
-                    <Text className='text-xl'>Remember</Text>
-                    <Image
-                        source={icons.toggle}
-                        style={{
-                            width: 35,
-                            height: 35,
-                        }}
-                        resizeMode='contain'
-                    />
-                </View>
-            </View>
-            <View className='mx-4 text-center mt-10 flex items-center'>
-                <Button
-                    title='Save Profile'
-                    onPress={() => navigator.navigate('Home')}
-                    color='rgb(15,23,42)'
-                />
-                <View className='flex flex-row items-center mt-6'>
-                    <Text className='text-xl mr-2'>Logout</Text>
-                    <Image
-                        source={icons.logout}
-                        style={{
-                            width: 20,
-                            height: 20,
-                        }}
-                        resizeMode='contain'
-                    />
-                </View>
-            </View>
+            <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                    dob: '',
+                    remember: 0
+                }}
+                onSubmit={values => {
+                    console.log(JSON.stringify(values))
+                }}>
+                {
+                    ({handleChange, handleBlur, handleSubmit, values}) =>
+                        <>
+                            <View className='mx-4 text-left mt-6'>
+                                <Text className='text-sm text-gray-500'>Name</Text>
+                                <View
+                                    className='flex flex-row justify-center items-center w-full border-solid border-b-2 border-gray-200 text-xl'>
+                                    <TextInput
+                                        className='flex flex-row w-full text-xl ms-4'
+                                        value={values.name}
+                                        onChangeText={handleChange('name')}
+                                        onBlur={handleBlur('name')}
+                                    />
+                                    <Image
+                                        source={icons.edit}
+                                        style={{
+                                            width: 20,
+                                            height: 20,
+                                        }}
+                                        resizeMode='contain'
+                                    />
+                                </View>
+                                <Text className='text-sm text-gray-500 mt-4'>Email</Text>
+                                <View
+                                    className='flex flex-row justify-center items-center w-full border-solid border-b-2 border-gray-200'>
+                                    <TextInput
+                                        className='text-xl ms-4'
+                                        value={values.email}
+                                        onChangeText={handleChange('email')}
+                                        onBlur={handleBlur('email')}
+                                    />
+                                </View>
+                                <Text className='text-sm text-gray-500 mt-4'>Date of Birth</Text>
+                                <View
+                                    className='flex flex-row justify-center items-center w-full border-solid border-b-2 border-gray-200'>
+                                    <TextInput
+                                        className='flex flex-row w-full text-xl ms-4'
+                                        value={values.dob}
+                                        onChangeText={handleChange('dob')}
+                                        onBlur={handleBlur('dob')}
+                                    />
+                                    <Image
+                                        source={icons.edit}
+                                        style={{
+                                            width: 20,
+                                            height: 20,
+                                        }}
+                                        resizeMode='contain'
+                                    />
+                                </View>
+                                <Text className='text-sm text-gray-500 mt-4'>Account Type</Text>
+                                <Text className='text-xl border-solid border-b-2 border-gray-200'>Moderator</Text>
+                                <Text className='text-sm text-gray-500 mt-4'>Fast Login</Text>
+                                <View className='flex flex-row justify-between border-solid border-b-2 border-gray-200'>
+                                    <Text className='text-xl'>Remember</Text>
+                                    <Image
+                                        source={icons.toggle}
+                                        style={{
+                                            width: 35,
+                                            height: 35,
+                                        }}
+                                        resizeMode='contain'
+                                    />
+                                </View>
+                            </View>
+                            <View className='mx-4 text-center mt-10 flex items-center'>
+                                <Button
+                                    title='Save Profile'
+                                    onPress={() => navigator.navigate('Home')}
+                                    color='rgb(15,23,42)'
+                                />
+                                <View className='flex flex-row items-center mt-6'>
+                                    <Text className='text-xl mr-2'>Logout</Text>
+                                    <Image
+                                        source={icons.logout}
+                                        style={{
+                                            width: 20,
+                                            height: 20,
+                                        }}
+                                        resizeMode='contain'
+                                    />
+                                </View>
+                            </View>
+                        </>
+                }
+            </Formik>
         </SafeAreaView>
     );
 };
