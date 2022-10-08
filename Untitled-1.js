481eb7e7804763237645f8d70063e47903e0e60c
PruthviPatel2001 <patelpruthvi.01221@gmail.com>
2022-10-08 15:06:23 +0530

    lastest

--- Untitled-1.js
+++ Untitled-1.js
@@ -165,3 +165,101 @@ import {
   
   export default Stream;
   
+
+  /////////////
+
+  import { AVPlaybackStatus, Video } from "expo-av";
+import {
+  ActivityIndicator,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
+import DroneList from "../components/HomeComp/DroneList";
+import { LivePlayer } from "react-native-live-stream";
+import WebView from "react-native-webview";
+import axios from "axios";
+import drones from "../Data/DroneData";
+import images from "../constants/images";
+
+const Stream = () => {
+  const [SelectedDroneId, setSelectedDroneId] = useState(1);
+  const [ViewImage, setViewImage] = useState();
+  const [ViewVideo, setViewVideo] = useState();
+  const [Loader, setLoader] = useState(false);
+  const [SelectedDrone, setSelectedDrone] = useState();
+  const [loader, setloader] = useState(true);
+  const [DroneData, setDroneData] = useState();
+  const [LiveLink, setLiveLink] = useState(
+    "https://0022-2401-4900-191f-46bb-8422-bf51-1120-4286.in.ngrok.io/video"
+  );
+  useEffect(() => {}, [SelectedDroneId]);
+
+  useEffect(() => {
+    const getDroneData = async () => {
+      const res = await axios.get("https://comikstorm.pythonanywhere.com/all");
+
+      res && setLoader(false);
+      setDroneData(res.data);
+    };
+
+    getDroneData();
+  }, []);
+
+  const video = React.useRef(null);
+  return (
+    <>
+      <View className="p-1 mt-8">
+        <Text
+          className="mt-14 px-4 text-xl "
+          style={{ fontFamily: "RobotoBold" }}
+        >
+          Live Stream
+        </Text>
+        <DroneList
+          styling={{ backgroundColor: "#17233B" }}
+          setSelectedDroneId={setSelectedDroneId}
+          data={DroneData}
+        />
+      </View>
+      <View style={styles.container} className="bg[#17233B]">
+        {loader ? (
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          <>
+          <WebView
+            source={{
+              html: '<iframe  width="100%" height="50%" src="https://1506-106-205-216-61.in.ngrok.io/video" frameborder="0" allow="autoplay; encrypted-media" ></iframe>',
+            }}
+            className="mt-24 bg-white"
+          />
+          </>
+        )}
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  video: {
+    alignSelf: "center",
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default Stream;
